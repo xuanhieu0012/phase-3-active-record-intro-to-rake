a984c407b695b888fb58e293d6cd3104a58419a5
Hieu <xuanhieu0012@gmail.com>
2021-09-27 00:21:27 -0500

    commit tree 6cf39b8bb10348f5d9e1a73f5995256c81b60d31

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,5 @@ Bundler.require
 
 require_relative "../lib/student"
 
+
 DB = { conn: SQLite3::Database.new("db/students.db") }
--- lib/student.rb
+++ lib/student.rb
@@ -22,6 +22,7 @@ class Student
         )
     SQL
     DB[:conn].execute(sql) 
+  
   end
 
   def self.drop_table
